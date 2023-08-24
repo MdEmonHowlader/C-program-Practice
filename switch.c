a006188702b5c91a6707610efb9298035351b78f
Emon <emon22205101732@diu.edu.bd>
2023-08-24 23:59:17 +0600

    update

---
+++ switch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int day=4;
+    switch(day)
+    {
+        case 1:
+        printf("Saturday");
+        break;
+
+        case 2:
+        printf("Sunday");
+        break;
+
+        case 3:
+        printf("Monday");
+        break;
+
+        case 4:
+        printf("Teusday");
+        break;
+
+        case 5:
+        printf("Wednesday");
+        break;
+
+        case 6:
+        printf("Thuresday");
+        break;
+
+        case 7:
+        printf("Friday");
+        break;
+    
+    }
+}
