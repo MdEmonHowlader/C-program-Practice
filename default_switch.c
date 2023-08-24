8ce300b62e8ae1a96d5ee472a14726022823f273
Emon <emon22205101732@diu.edu.bd>
2023-08-25 00:08:03 +0600

    default

---
+++ default_switch.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int day=4;
+    switch (day)
+    {
+    case 5:
+        printf("Data is found");
+        break;
+
+    case 7:
+    printf("Today is Sunday");
+    break;
+    
+    default:
+    printf("data is not found");
+        break;
+    }
+    return 0;
+}
